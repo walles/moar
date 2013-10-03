1d67a63c712df245ac154e5d35e3c013a1976534
Johan Walles <johan.walles@gmail.com>
2013-10-03 21:27:24 +0200

    Scroll using up / down arrow keys

--- src/moar.rb
+++ src/moar.rb
@@ -6,8 +6,10 @@ class Moar
   include Curses
 
   def initialize(file)
+    @first_line = 0
     @file = file
     @lines = IO.readlines(file)
+    @last_key = 0
   end
 
   def draw_screen()
@@ -15,14 +17,13 @@ class Moar
     setpos(0, 0)
 
     attrset(A_NORMAL)
-    first_line = 0
-    last_line = lines - 2
-    @lines[first_line..last_line].each do |line|
+    last_line = @first_line + lines - 2
+    @lines[@first_line..last_line].each do |line|
       addstr(line)
     end
 
     attrset(A_REVERSE)
-    addstr("Lines #{first_line + 1}-#{last_line + 1}")
+    addstr("Lines #{@first_line + 1}-#{last_line + 1}, key=#{@last_key}")
 
     refresh()
   end
@@ -30,6 +31,7 @@ class Moar
   def run
     init_screen
     noecho
+    stdscr.keypad(true)
 
     begin
       crmode
@@ -40,9 +42,17 @@ class Moar
         case key
         when ?q.ord
           break
-        when KEY_RESIZE
+        when Key::RESIZE
           draw_screen()
+        when Key::DOWN
+          @first_line += 1
+        when Key::UP
+          @first_line -= 1
         end
+
+        @first_line = 0 if @first_line < 0
+
+        @last_key = key
       end
     ensure
       close_screen
