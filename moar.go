2225ef826b5adee31953b868ba7cf955cb42f338
Johan Walles <johan.walles@gmail.com>
2019-06-09 19:40:35 +0200

    Rudimentary command line parsing

--- moar.go
+++ moar.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"os"
 
@@ -13,6 +14,12 @@ func main() {
 	stdoutIsRedirected := !terminal.IsTerminal(int(os.Stdout.Fd()))
 	if stdinIsRedirected && stdoutIsRedirected {
 		io.Copy(os.Stdout, os.Stdin)
+	}
+
+	if len(os.Args) != 2 {
+		// FIXME: Improve this message
+		fmt.Fprintf(os.Stderr, "ERROR: Expected exactly one parameter, got: %v\n", os.Args[1:])
+		os.Exit(1)
 	}
 
 	// FIXME: If first arg is a file name and stdout is a non-TTY, pump file onto stdout
