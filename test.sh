af23822bc2b4380981d08f9e41bcf3c32442109b
Johan Walles <johan.walles@gmail.com>
2019-06-09 22:03:25 +0200

    Put integration tests in tests.sh
    This makes the testing code a bit simpler.

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -e -o pipefail
 
 # Ensure we can cross compile
 GOOS=linux GOARCH=386 go build
@@ -9,12 +9,30 @@ GOOS=darwin GOARCH=amd64 go build
 # Unit tests first...
 go test
 
-# ... then integration.
+# ... then Verify sending the output to a file
 go build
-for TEST in integration-tests/*.sh ; do
-    echo "Executing: $(basename "$TEST")..."
-    "$TEST"
-done
+
+RESULT="$(mktemp)"
+function cleanup {
+  rm -rf "$RESULT"
+}
+trap cleanup EXIT
+
+echo Running to-file redirection tests...
+
+echo Test reading from redirected stdin, writing to redirected stdout...
+./moar < moar.go > "$RESULT"
+diff -u moar.go "$RESULT"
+
+echo Test redirecting a file by name into file by redirecting stdout...
+./moar moar.go > "$RESULT"
+diff -u moar.go "$RESULT"
+
+echo Test redirecting non-existing file by name into redirected stdout...
+if ./moar does-not-exist >& /dev/null ; then
+    echo ERROR: Should have failed on non-existing input file name
+    exit 1
+fi
 
 echo
 echo "All tests passed!"
