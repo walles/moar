e1eb4fd51e4dc085e805b1174645324eac2e0e4d
Johan Walles <johan.walles@gmail.com>
2019-10-27 21:43:33 +0100

    Fix an assertion test

--- m/ansiTokenizer_test.go
+++ m/ansiTokenizer_test.go
@@ -84,44 +84,44 @@ func TestConsumeCompositeColorBadPrefix(t *testing.T) {
 	// Example from: https://github.com/walles/moar/issues/14
 	_, color, err := consumeCompositeColor([]string{"29"}, 0)
 	assert.Equal(t, err.Error(), "Unknown start of color sequence <29>, expected 38 (foreground) or 48 (background): <CSI 29m>")
-	assert.Equal(t, color, nil)
+	assert.Assert(t, color == nil)
 
 	// Same test but mid-sequence, with initial index > 0
 	_, color, err = consumeCompositeColor([]string{"whatever", "29"}, 1)
 	assert.Equal(t, err.Error(), "Unknown start of color sequence <29>, expected 38 (foreground) or 48 (background): <CSI 29m>")
-	assert.Equal(t, color, nil)
+	assert.Assert(t, color == nil)
 }
 
 func TestConsumeCompositeColorBadType(t *testing.T) {
 	_, color, err := consumeCompositeColor([]string{"38", "4"}, 0)
 	// https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
 	assert.Equal(t, err.Error(), "Unknown color type <4>, expected 5 (8 bit color) or 2 (24 bit color): <CSI 38;4m>")
-	assert.Equal(t, color, nil)
+	assert.Assert(t, color == nil)
 
 	// Same test but mid-sequence, with initial index > 0
 	_, color, err = consumeCompositeColor([]string{"whatever", "38", "4"}, 1)
 	assert.Equal(t, err.Error(), "Unknown color type <4>, expected 5 (8 bit color) or 2 (24 bit color): <CSI 38;4m>")
-	assert.Equal(t, color, nil)
+	assert.Assert(t, color == nil)
 }
 
 func TestConsumeCompositeColorIncomplete8Bit(t *testing.T) {
 	_, color, err := consumeCompositeColor([]string{"38", "5"}, 0)
 	assert.Equal(t, err.Error(), "Incomplete 8 bit color sequence: <CSI 38;5m>")
-	assert.Equal(t, color, nil)
+	assert.Assert(t, color == nil)
 
 	// Same test, mid-sequence
 	_, color, err = consumeCompositeColor([]string{"whatever", "38", "5"}, 1)
 	assert.Equal(t, err.Error(), "Incomplete 8 bit color sequence: <CSI 38;5m>")
-	assert.Equal(t, color, nil)
+	assert.Assert(t, color == nil)
 }
 
 func TestConsumeCompositeColorIncomplete24Bit(t *testing.T) {
 	_, color, err := consumeCompositeColor([]string{"38", "2", "10", "20"}, 0)
 	assert.Equal(t, err.Error(), "Incomplete 24 bit color sequence, expected N8;2;R;G;Bm: <CSI 38;2;10;20m>")
-	assert.Equal(t, color, nil)
+	assert.Assert(t, color == nil)
 
 	// Same test, mid-sequence
 	_, color, err = consumeCompositeColor([]string{"whatever", "38", "2", "10", "20"}, 1)
 	assert.Equal(t, err.Error(), "Incomplete 24 bit color sequence, expected N8;2;R;G;Bm: <CSI 38;2;10;20m>")
-	assert.Equal(t, color, nil)
+	assert.Assert(t, color == nil)
 }
