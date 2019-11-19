9a2ab6df98991ebe8aa8038401990706ed72394d
Johan Walles <johan.walles@gmail.com>
2019-11-19 16:53:17 +0100

    Add test for reading long lines
    Change-Id: Ib24518c681f1ffc93192a985cd573fa37006d1f8

--- m/reader_test.go
+++ m/reader_test.go
@@ -3,6 +3,7 @@ package m
 import (
 	"io/ioutil"
 	"math"
+	"os"
 	"os/exec"
 	"path"
 	"runtime"
@@ -140,6 +141,33 @@ func TestGetLines(t *testing.T) {
 		_TestGetLines(t, reader)
 		_TestGetLineCount(t, reader)
 	}
+}
+
+func TestGetLongLine(t *testing.T) {
+	file := "../sample-files/very-long-line.txt"
+	reader, err := NewReaderFromFilename(file)
+	if err != nil {
+		panic(err)
+	}
+	if err := reader._Wait(); err != nil {
+		panic(err)
+	}
+
+	lines := reader.GetLines(1, 5)
+	assert.Equal(t, lines.firstLineOneBased, 1)
+	assert.Equal(t, len(lines.lines), 1)
+
+	stat, err := os.Stat(file)
+	if err != nil {
+		panic(err)
+	}
+	fileSize := stat.Size()
+
+	line := lines.lines[0]
+	assert.Equal(t, len(line), fileSize)
+
+	assert.Assert(t, strings.HasPrefix(line, "1 2 3 4"))
+	assert.Assert(t, strings.HasSuffix(line, "0123456789"))
 }
 
 func _GetReaderWithLineCount(totalLines int) *Reader {
