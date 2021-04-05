17bc66a8e3bebe9ff589fa31b70dab8cb65245d3
Steven Penny <srpen6@gmail.com>
2021-04-05 16:30:33 -0500

    Add DeInit variable
    This allows users to control whether the screen is cleared on return. Default is
    to clear the screen. Example use:
    
        r := m.NewReaderFromText("dir", strings.Repeat("north\n", 50))
        m.DeInit = false
        m.Page(r)
    
    Fixes #33

--- m/embed-api.go
+++ m/embed-api.go
@@ -2,6 +2,10 @@ package m
 
 import "github.com/gdamore/tcell/v2"
 
+// If true, the Page function will clear the screen on return. If false, the
+// Page function will clear the last line, and show the cursor.
+var DeInit = true
+
 // Page displays text in a pager.
 //
 // The reader parameter can be constructed using one of:
@@ -17,7 +21,18 @@ func Page(reader *Reader) error {
 		// Screen setup failed
 		return e
 	}
-	defer screen.Fini()
+	defer func() {
+		if DeInit {
+			screen.Fini()
+		} else {
+			w, h := screen.Size()
+			screen.ShowCursor(0, h - 1)
+			for x := 0; x < w; x++ {
+				screen.SetContent(x, h - 1, ' ', nil, tcell.StyleDefault)
+			}
+			screen.Show()
+		}
+	}()
 
 	NewPager(reader).StartPaging(screen)
 	return nil
