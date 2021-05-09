346804f23ce1d265a66e1545f3ef568af438ec1c
Johan Walles <johan.walles@gmail.com>
2021-05-09 21:44:24 +0200

    Add initial tests
    They don't pass and we should probably have more of them.

---
+++ m/lineReader_test.go
@@ -0,0 +1,60 @@
+package m
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestEmpty(t *testing.T) {
+	reader := strings.NewReader("")
+	lineReader := NewLineReader(reader)
+	line, err := lineReader.GetLine()
+	if err != nil {
+		panic(err)
+	}
+	if line != nil {
+		t.Errorf("Line should have been nil: <%s>", *line)
+	}
+}
+
+func TestOneLineTrailingNewline(t *testing.T) {
+	reader := strings.NewReader("apor\n")
+	lineReader := NewLineReader(reader)
+
+	line, err := lineReader.GetLine()
+	if err != nil {
+		panic(err)
+	}
+	if *line != "apor" {
+		t.Errorf("Unexpected line contents: <%s>", *line)
+	}
+
+	line, err = lineReader.GetLine()
+	if err != nil {
+		panic(err)
+	}
+	if line != nil {
+		t.Errorf("Second line should have been nil: <%s>", *line)
+	}
+}
+
+func TestOneLineWithoutTrailingNewline(t *testing.T) {
+	reader := strings.NewReader("apor")
+	lineReader := NewLineReader(reader)
+
+	line, err := lineReader.GetLine()
+	if err != nil {
+		panic(err)
+	}
+	if *line != "apor" {
+		t.Errorf("Unexpected line contents: <%s>", *line)
+	}
+
+	line, err = lineReader.GetLine()
+	if err != nil {
+		panic(err)
+	}
+	if line != nil {
+		t.Errorf("Second line should have been nil: <%s>", *line)
+	}
+}
