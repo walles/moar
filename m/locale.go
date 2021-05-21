1d94f25e2db735c0d6b835fc6d8b662faeb879d3
Johan Walles <johan.walles@gmail.com>
2021-05-21 07:02:02 +0200

    Get locale from environment

--- m/locale.go
+++ m/locale.go
@@ -1,6 +1,9 @@
 package m
 
 import (
+	"os"
+	"strings"
+
 	"golang.org/x/text/language"
 	"golang.org/x/text/message"
 )
@@ -8,8 +11,40 @@ import (
 // Like "fmt" but for the current locale
 var lfmt = getLocaleFormatter()
 
+func parseLocaleString(localeString string) *language.Tag {
+	dotIndex := strings.IndexRune(localeString, '.')
+	if dotIndex >= 0 {
+		// Turns "sv_SE.UTF-8" into "sv_SE"
+		localeString = localeString[:dotIndex]
+	}
+
+	candidate, err := language.Parse(localeString)
+	if err != nil {
+		return nil
+	}
+
+	if candidate == language.Und {
+		return nil
+	}
+
+	return &candidate
+}
+
 func getLocaleFormatter() *message.Printer {
-	// FIXME: Check LANG and LC_NUMERIC and parse it: https://pkg.go.dev/golang.org/x/text/message
-	locale := language.English
+	// Prefer LC_NUMERIC value, otherwise LANG value
+	locale := language.Und
+
+	candidate := parseLocaleString(os.Getenv("LANG"))
+	if candidate != nil {
+		// Overwrite locale selection with LANG parse result
+		locale = *candidate
+	}
+
+	candidate = parseLocaleString(os.Getenv("LC_NUMERIC"))
+	if candidate != nil {
+		// Overwrite locale selection with LC_NUMERIC parse result
+		locale = *candidate
+	}
+
 	return message.NewPrinter(locale)
 }
