f0283e9e57bfe7decdc6c858509df2396e6b83d8
Johan Walles <johan.walles@gmail.com>
2021-05-22 10:41:34 +0200

    Implement simplistic wrapping

--- m/linewrapper.go
+++ m/linewrapper.go
@@ -3,5 +3,17 @@ package m
 import "github.com/walles/moar/twin"
 
 func wrapLine(width int, line []twin.Cell) [][]twin.Cell {
-	return [][]twin.Cell{line}
+	wrapped := make([][]twin.Cell, 0, len(line)/width)
+	for len(line) > width {
+		firstPart := line[:width]
+		wrapped = append(wrapped, firstPart)
+
+		line = line[width:]
+	}
+
+	if len(line) > 0 {
+		wrapped = append(wrapped, line)
+	}
+
+	return wrapped
 }
--- m/linewrapper_test.go
+++ m/linewrapper_test.go
@@ -1,6 +1,7 @@
 package m
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/walles/moar/twin"
@@ -15,23 +16,33 @@ func tokenize(input string) []twin.Cell {
 func TestEnoughRoomNoWrapping(t *testing.T) {
 	toWrap := tokenize("This is a test")
 	wrapped := wrapLine(20, toWrap)
-	assert.Equal(t, wrapped, [][]twin.Cell{toWrap})
+	assert.Assert(t, reflect.DeepEqual(wrapped, [][]twin.Cell{toWrap}))
 }
 
 func TestSimpleWrap(t *testing.T) {
 	toWrap := tokenize("This is a test")
 	wrapped := wrapLine(12, toWrap)
-	assert.Equal(t, wrapped, [][]twin.Cell{
+	assert.Assert(t, reflect.DeepEqual(wrapped, [][]twin.Cell{
 		tokenize("This is a"),
 		tokenize("test"),
-	})
+	}))
 }
 
 func TestWordLongerThanLine(t *testing.T) {
 	toWrap := tokenize("intermediary")
 	wrapped := wrapLine(6, toWrap)
-	assert.Equal(t, wrapped, [][]twin.Cell{
+	assert.Assert(t, reflect.DeepEqual(wrapped, [][]twin.Cell{
 		tokenize("interm"),
 		tokenize("ediary"),
-	})
+	}))
 }
+
+// FIXME: Test wrapping with multiple consecutive spaces
+
+// FIXME: Test wrapping on single dashes
+
+// FIXME: Test wrapping with double dashes (not sure what we should do with those)
+
+// FIXME: Test wrapping formatted strings, is there formatting that should affect the wrapping
+
+// FIXME: Test wrapping with trailing whitespace
