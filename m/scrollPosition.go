8bf34a2ab070dd99c5908bd56544e67f70add11e
Johan Walles <johan.walles@gmail.com>
2022-02-19 19:40:52 +0100

    Simplify scroll position handling
    Requiring more clipping logic during rendering.

--- m/scrollPosition.go
+++ m/scrollPosition.go
@@ -4,55 +4,22 @@ type scrollPosition struct {
 	// Line number in the input stream, zero based
 	lineNumber int
 
-	// If a file line has been broken into two screen lines by wrapping, the
-	// first screen line has wrapIndex 0, and the second one 1.
-	wrapIndex int
-}
-
-func (s scrollPosition) sameOrBefore(otherPosition scrollPosition) bool {
-	if s.lineNumber <= otherPosition.lineNumber {
-		return true
-	}
-
-	if s.lineNumber > otherPosition.lineNumber {
-		return false
-	}
-
-	// Invariant: Both positions are on the same line
-
-	return s.wrapIndex <= otherPosition.wrapIndex
+	// Scroll this many screen lines before rendering. Can be negative.
+	deltaScreenLines int
 }
 
 // Create a new position, scrolled towards the end of the file
-func (s scrollPosition) previousLine(pager *Pager, scrollDistance int) scrollPosition {
-	// FIXME: Scroll up, taking line wrapping into account
+func (s scrollPosition) previousLine(scrollDistance int) scrollPosition {
+	return scrollPosition{
+		lineNumber:       s.lineNumber,
+		deltaScreenLines: s.deltaScreenLines - 1,
+	}
 }
 
 // Create a new position, scrolled towards the end of the file
-func (s scrollPosition) nextLine(pager *Pager, scrollDistance int) scrollPosition {
-	// FIXME: Scroll down, taking line wrapping into account
-
-	returnMe := s
-
-	for i := 0; i < scrollDistance; i++ {
-		// FIXME: Split the current line into []RenderedLines
-		rendered := renderLine(fixme_rawCurrentLineContents)
-
-		// Move to the next wrapIndex
-		returnMe.wrapIndex += 1
-
-		// If our wrapIndex > the maximum RenderedLine wrapIndex, move to the next
-		// input line
-		if returnMe.wrapIndex > len(rendered) {
-			if returnMe.lineNumber == pager.reader.GetLineCount()-1 {
-				// We're already at the last line, can't scroll any further
-				return returnMe
-			}
-
-			returnMe.lineNumber += 1
-			returnMe.wrapIndex = 0
-		}
+func (s scrollPosition) nextLine(scrollDistance int) scrollPosition {
+	return scrollPosition{
+		lineNumber:       s.lineNumber,
+		deltaScreenLines: s.deltaScreenLines + 1,
 	}
-
-	return returnMe
 }
