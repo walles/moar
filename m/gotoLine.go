d763846f5475a6ee3bc0a51504da17c5655b3c87
Johan Walles <johan.walles@gmail.com>
2022-05-01 16:41:24 +0200

    Go to line logic placeholder

---
+++ m/gotoLine.go
@@ -0,0 +1,17 @@
+package m
+
+import (
+	"github.com/walles/moar/twin"
+)
+
+func (p *Pager) addGotoLineFooter() {
+	FIXME() // Implement based on addSearchFooter
+}
+
+func (p *Pager) onGotoLineKey(key twin.KeyCode) {
+	FIXME() // Implement based on onSearchKey
+}
+
+func (p *Pager) onGotoLineRune(char rune) {
+	FIXME() // Implement based on onSearchRune
+}
