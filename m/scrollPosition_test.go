d0465e751eee8792fccc52b00e852755eefc5219
Johan Walles <johan.walles@gmail.com>
2023-12-01 07:00:39 +0100

    Remove unneeded code from the test

--- m/scrollPosition_test.go
+++ m/scrollPosition_test.go
@@ -12,25 +12,18 @@ func TestCanonicalize1000(t *testing.T) {
 	pager := Pager{}
 	pager.screen = twin.NewFakeScreen(100, 100)
 	pager.reader = NewReaderFromText("test", strings.Repeat("a\n", 2000))
-	pager.ShowStatusBar = true
 	pager.ShowLineNumbers = true
-	pager.scrollPosition = newScrollPosition("TestCanonicalize1000")
+
+	// FIXME: Does this matter for reproducing the crash? Recheck after we fix the issue!
+	pager.ShowStatusBar = false
 
 	scrollPosition := scrollPosition{
 		internalDontTouch: scrollPositionInternal{
 			lineNumberOneBased: 1000,
 			deltaScreenLines:   0,
-			name:               "Fake scroll position",
+			name:               "TestCanonicalize1000",
 			canonicalizing:     false,
-			canonical: scrollPositionCanonical{
-				width:              0,
-				height:             0,
-				showLineNumbers:    false,
-				showStatusBar:      false,
-				wrapLongLines:      false,
-				lineNumberOneBased: 0,
-				deltaScreenLines:   0,
-			},
+			canonical:          scrollPositionCanonical{},
 		},
 	}
 
