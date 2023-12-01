a87125ca0c30da61dfd14e0dabb8c5efc6c7fa85
Johan Walles <johan.walles@gmail.com>
2023-12-01 09:06:26 +0100

    Repro situation from the real world crash
    And it panics again!

--- m/scrollPosition_test.go
+++ m/scrollPosition_test.go
@@ -11,23 +11,28 @@ import (
 // Repro for: https://github.com/walles/moar/issues/166
 func TestCanonicalize1000(t *testing.T) {
 	pager := Pager{}
-	pager.screen = twin.NewFakeScreen(100, 100)
+	pager.screen = twin.NewFakeScreen(100, 60)
 	pager.reader = NewReaderFromText("test", strings.Repeat("a\n", 2000))
 	pager.ShowLineNumbers = true
-
-	// FIXME: Does this matter for reproducing the crash? Recheck after we fix the issue!
 	pager.ShowStatusBar = true
-
 	pager.scrollPosition = scrollPosition{
 		internalDontTouch: scrollPositionInternal{
-			lineNumberOneBased: 901,
+			lineNumberOneBased: 941,
 			deltaScreenLines:   0,
-			name:               "TestCanonicalize1000",
+			name:               "findFirstHit",
 			canonicalizing:     false,
-			canonical:          scrollPositionCanonical{},
 		},
 	}
-	lineNumberOneBased := pager.scrollPosition.lineNumberOneBased(&pager)
+
+	lastVisiblePosition := scrollPosition{
+		internalDontTouch: scrollPositionInternal{
+			lineNumberOneBased: 999,
+			deltaScreenLines:   0,
+			name:               "Last Visible Position",
+		},
+	}
+
+	lineNumberOneBased := lastVisiblePosition.lineNumberOneBased(&pager)
 
 	assert.Equal(t, lineNumberOneBased, 42)
 }
