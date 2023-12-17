4eafa5586e7ac345453fdb8a03e5fda75b995de7
Johan Walles <johan.walles@gmail.com>
2023-12-17 21:29:34 +0100

    Pass some more tests
    One to go.

--- twin/palette256_test.go
+++ twin/palette256_test.go
@@ -21,3 +21,19 @@ func TestColorToRgbInThe6x6Cube(t *testing.T) {
 	assert.Equal(t, g, float64(0x87)/255.0)
 	assert.Equal(t, b, float64(0x5f)/255.0)
 }
+
+func TestColorToRgbStart6x6Cube(t *testing.T) {
+	r, g, b := color256ToRGB(16)
+
+	assert.Equal(t, r, float64(0x00)/255.0)
+	assert.Equal(t, g, float64(0x00)/255.0)
+	assert.Equal(t, b, float64(0x00)/255.0)
+}
+
+func TestColorRgbEnd6x6Cube(t *testing.T) {
+	r, g, b := color256ToRGB(231)
+
+	assert.Equal(t, r, float64(0xff)/255.0)
+	assert.Equal(t, g, float64(0xff)/255.0)
+	assert.Equal(t, b, float64(0xff)/255.0)
+}
