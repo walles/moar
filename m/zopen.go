cc4d756a36301a420256b36c1f056a8599190c05
Johan Walles <johan.walles@gmail.com>
2024-01-08 13:26:05 +0100

    Create a function for opening with decompression

---
+++ m/zopen.go
@@ -0,0 +1,33 @@
+package m
+
+import (
+	"io"
+	"os"
+
+	"github.com/mholt/archiver/v4"
+)
+
+func zOpen(filename string) (io.ReadCloser, error) {
+	format, _, err := archiver.Identify(filename, nil)
+	if err == archiver.ErrNoMatch {
+		// Not compressed, just open the file
+		return os.Open(filename)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	if decompressor, ok := format.(archiver.Decompressor); ok {
+		fileStream, err := os.Open(filename)
+		if err != nil {
+			return nil, err
+		}
+
+		return decompressor.OpenReader(fileStream)
+	}
+
+	// An archive of some sort, not much we can do with that, just hand the user
+	// the binary gibberish.
+	return os.Open(filename)
+}
