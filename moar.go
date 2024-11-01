4146c17ea08b9c1ed6af609089114d7973d87af4
Johan Walles <johan.walles@gmail.com>
2024-11-01 22:28:20 +0100

    Document the --no-reformat command line option

--- moar.go
+++ moar.go
@@ -331,7 +331,7 @@ func pagerFromArgs(
 
 	noLineNumbers := flagSet.Bool("no-linenumbers", noLineNumbersDefault(), "Hide line numbers on startup, press left arrow key to show")
 	noStatusBar := flagSet.Bool("no-statusbar", false, "Hide the status bar, toggle with '='")
-	noFormat := flagSet.Bool("no-format", false, "Never format the input (but keep highlighting)")
+	noReFormat := flagSet.Bool("no-reformat", false, "Never reformat the input (but keep highlighting)")
 	quitIfOneScreen := flagSet.Bool("quit-if-one-screen", false, "Don't page if contents fits on one screen")
 	noClearOnExit := flagSet.Bool("no-clear-on-exit", false, "Retain screen contents when exiting moar")
 	statusBarStyle := flagSetFunc(flagSet, "statusbar", m.STATUSBAR_STYLE_INVERSE,
@@ -461,7 +461,7 @@ func pagerFromArgs(
 	}
 
 	var reader *m.Reader
-	shouldFormat := !*noFormat
+	shouldFormat := !*noReFormat
 	if stdinIsRedirected {
 		// Display input pipe contents
 		reader = m.NewReaderFromStream("", os.Stdin, formatter, m.ReaderOptions{Lexer: *lexer, ShouldFormat: shouldFormat})
