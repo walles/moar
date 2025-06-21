ee8eb15dfe43aa1e9863ec60abaee9c1207e45df
Johan Walles <johan.walles@gmail.com>
2025-06-21 17:22:27 +0200

    WIP Try showing the last line only
    Doesn't work, crash moar like this:
    1. "./moar.sh /etc/services"
    2. Press &
    3. Resize the terminal window.

--- m/filteringReader.go
+++ m/filteringReader.go
@@ -5,7 +5,7 @@ import "github.com/walles/moar/m/linemetadata"
 // FIXME: This reader should filter its input lines based on the search query
 // from the pager.
 //
-// For starters, let's just make it return at most one line and see what happens.
+// For starters, let's just make it always return the last line and see what happens.
 
 type FilteringReader struct {
 	backingReader Reader
@@ -16,18 +16,38 @@ func (f FilteringReader) GetLineCount() int {
 }
 
 func (f FilteringReader) GetLine(index linemetadata.Index) *NumberedLine {
-	if index.Index() > 0 {
+	if index.Index() != 0 {
 		return nil
 	}
-	return f.backingReader.GetLine(index)
+	if f.backingReader.GetLineCount() == 0 {
+		return nil
+	}
+	return f.backingReader.GetLine(linemetadata.IndexFromZeroBased(f.backingReader.GetLineCount() - 1))
 }
 
+
+This code crashes if you do "./moar.sh /etc/services", press & and then resize the window.
+
+
 func (f FilteringReader) GetLines(firstLine linemetadata.Index, wantedLineCount int) *InputLines {
-	if firstLine.Index() > 0 {
+	if firstLine.Index() != 0 || f.backingReader.GetLineCount() == 0 {
 		return &InputLines{
 			statusText: "FIXME: This is a dummy filter to demonstrate the interface",
 		}
 	}
 
-	return f.backingReader.GetLines(firstLine, min(wantedLineCount, 1))
+	result := f.backingReader.GetLines(linemetadata.IndexFromZeroBased(f.backingReader.GetLineCount()-1), min(wantedLineCount, 1))
+	if len(result.lines) == 0 {
+		return &InputLines{
+			statusText: "Dummy filter found no lines",
+		}
+	}
+
+	line := result.lines[0]
+	line.index = linemetadata.IndexFromZeroBased(0)
+	return &InputLines{
+		lines:      []*NumberedLine{line},
+		statusText: "FIXME: This is a dummy filter to demonstrate the interface",
+		firstLine:  linemetadata.IndexFromZeroBased(0),
+	}
 }
