4f3974a77b0f576486818c9f48978ce53916da05
Johan Walles <johan.walles@gmail.com>
2025-06-22 09:28:38 +0200

    Simplify the dummy filtering code

--- m/filteringReader.go
+++ m/filteringReader.go
@@ -1,6 +1,10 @@
 package m
 
-import "github.com/walles/moar/m/linemetadata"
+import (
+	"sync"
+
+	"github.com/walles/moar/m/linemetadata"
+)
 
 // FIXME: This reader should filter its input lines based on the search query
 // from the pager.
@@ -11,43 +15,36 @@ type FilteringReader struct {
 	backingReader Reader
 }
 
+var mockSingleLine = &NumberedLine{
+	index: linemetadata.IndexFromZeroBased(0),
+	line: &Line{
+		raw:  "This is dummy response line from the filtering reader",
+		lock: sync.Mutex{},
+	},
+}
+
 func (f FilteringReader) GetLineCount() int {
-	return min(f.backingReader.GetLineCount(), 1)
+	return 1
 }
 
 func (f FilteringReader) GetLine(index linemetadata.Index) *NumberedLine {
 	if index.Index() != 0 {
 		return nil
 	}
-	if f.backingReader.GetLineCount() == 0 {
-		return nil
-	}
-	return f.backingReader.GetLine(linemetadata.IndexFromZeroBased(f.backingReader.GetLineCount() - 1))
+
+	return mockSingleLine
 }
 
-
-This code crashes if you do "./moar.sh /etc/services", press & and then resize the window.
-
-
 func (f FilteringReader) GetLines(firstLine linemetadata.Index, wantedLineCount int) *InputLines {
-	if firstLine.Index() != 0 || f.backingReader.GetLineCount() == 0 {
+	if firstLine.Index() != 0 {
 		return &InputLines{
-			statusText: "FIXME: This is a dummy filter to demonstrate the interface",
+			statusText: "Dummy status text, index out of bounds",
 		}
 	}
 
-	result := f.backingReader.GetLines(linemetadata.IndexFromZeroBased(f.backingReader.GetLineCount()-1), min(wantedLineCount, 1))
-	if len(result.lines) == 0 {
-		return &InputLines{
-			statusText: "Dummy filter found no lines",
-		}
-	}
-
-	line := result.lines[0]
-	line.index = linemetadata.IndexFromZeroBased(0)
 	return &InputLines{
-		lines:      []*NumberedLine{line},
-		statusText: "FIXME: This is a dummy filter to demonstrate the interface",
+		lines:      []*NumberedLine{mockSingleLine},
+		statusText: "Dummy status text, returning single line",
 		firstLine:  linemetadata.IndexFromZeroBased(0),
 	}
 }
